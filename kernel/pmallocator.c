ea11d91d1c82af28198ee37fed02a39838c7ae57
acpass <a867123276@163.com>
2023-05-03 20:33:30 +0800

    fix: fill allocated page with junk

--- kernel/pmallocator.c
+++ kernel/pmallocator.c
@@ -35,6 +35,9 @@ void *pm_alloc(void)
     if(__sync_bool_compare_and_swap(&physical_mem.head, allocated_node, allocated_node->next))
       break;
   }
+
+  allocated_node->next = (void *)0;
+
   return (void *)allocated_node;
 }
 
@@ -44,6 +47,8 @@ void pm_free(pm_page_node *node)
     PANIC("PM_FREE NOT ALIGNED");
 
   pm_page_node *old;
+
+  memset((void *)node, PAGE_SIZE, 0);
 
   while (1)
   {
